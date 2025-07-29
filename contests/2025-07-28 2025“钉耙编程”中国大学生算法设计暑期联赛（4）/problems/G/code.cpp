647fa887d2121dcfbf3b6e963c2df5a504009172
Appleblue17 <Appleblue17@users.noreply.github.com>
2025-07-29 01:46:27 +0000

    deploy: 8db6dc975605c809dca168ae7011acf7c235961f

--- contests/2025-07-28 2025“钉耙编程”中国大学生算法设计暑期联赛（4）/problems/G/code.cpp
+++ contests/2025-07-28 2025“钉耙编程”中国大学生算法设计暑期联赛（4）/problems/G/code.cpp
@@ -12,51 +12,6 @@ template <typename T>inline void read(T &x){
 int T, n, m;
 int a[N];
 
-int L[N],R[N];
-
-void del(int pos){
-    int x=a[pos];
-    
-    if(x){
-        L[pos]=R[pos]=pos;
-    }
-    else{
-        if(pos-1>=1 && !a[pos-1]){
-            R[pos-1]=pos-1;
-            R[L[pos-1]]=pos-1;
-        }
-        if(pos+1<=n && !a[pos+1]){
-            L[pos+1]=pos+1;
-            L[R[pos+1]]=pos+1;
-        }
-        L[pos]=R[pos]=pos;
-    }
-}
-void add(int pos){
-    int x=a[pos];
-    
-    if(x){
-        L[pos]=R[pos]=pos;
-    }
-    else{
-        L[pos]=R[pos]=pos;
-        if(pos-1>=1 && !a[pos-1]) L[pos]=L[pos-1];
-        if(pos+1<=n && !a[pos+1]) R[pos]=R[pos+1];
-        if(pos-1>=1 && !a[pos-1]){
-            R[pos-1]=R[pos];
-            R[L[pos-1]]=R[pos];
-        }
-        if(pos+1<=n && !a[pos+1]){
-            L[pos+1]=L[pos];
-            L[R[pos+1]]=L[pos];
-        }
-    }
-}
-int ask(int pos){
-    if(pos==n || a[pos+1]) return pos+1;
-    return R[pos+1]+1;
-}
-
 struct Tree{
     bool tr[N<<2];
     void build(int p, int l, int r, int tp){
@@ -111,242 +66,121 @@ struct Tree{
     bool get(int p, int l, int r, int L, int R){
         if(L>R) return 0;
         if(L<=l&&r<=R) return tr[p];
-        int mid=(l+r)>>1, ret=0;
+        int mid=(l+r)>>1; bool ret=0;
         if(L<=mid) ret=get(p<<1, l, mid, L, R);
         if(R>mid) ret|=get(p<<1|1, mid+1, r, L, R);
         return ret;
     }
 }T1, T2, T3;
-struct info{
-    int l, r, st, len, ans;
-}tr[N<<2];
-int sz, sz2;
-inline info operator +(info x, info y){
-    info z;
-    z.l=x.l; z.r=y.r;
-    z.ans=max(x.ans, y.ans);
-    if(x.st==0){
-        z.st=y.st;
-        z.len=y.len;
-        return z;
-    }
-    if(x.len==1){
-        if(a[y.l]!=2){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l==y.r){
-            z.st=x.st;
-            z.len=2;
-            return z;
-        }
-        if(a[y.l+1]!=1){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l+1==y.r){
-            z.st=x.st;
-            z.len=3;
-            return z;
-        }
-        sz=min(ask(y.l+1), y.r+1)-(y.l+1)-1;
-        if(sz>x.st-3){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l+2+sz-1==y.r){
-            z.st=x.st;
-            z.len=3+sz;
-            return z;
-        }
-        if(sz!=x.st-3){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(a[y.l+2+sz]!=1){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l+2+sz==y.r){
-            z.st=x.st;
-            z.len=3+sz+1;
-            return z;
-        }
-        sz2=min(ask(y.l+2+sz), y.r+1)-(y.l+2+sz)-1;
-        if(sz2>=3){
-            z.ans=max(z.ans, x.st+4);
-            z.st=y.st; z.len=y.len;
-            return z;
-        }
-        if(y.l+2+sz+1+sz2-1==y.r){
-            z.st=x.st;
-            z.len=3+sz+1+sz2;
-            return z;
-        }
-        z.st=y.st;
-        z.len=y.len;
-        return z;
-    }
-    if(x.len==2){
-        if(a[y.l]!=1){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l==y.r){
-            z.st=x.st;
-            z.len=3;
-            return z;
-        }
-        sz=min(ask(y.l), y.r+1)-(y.l)-1;
-        if(sz>x.st-3){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l+1+sz-1==y.r){
-            z.st=x.st;
-            z.len=3+sz;
-            return z;
-        }
-        if(sz!=x.st-3){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(a[y.l+1+sz]!=1){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l+1+sz==y.r){
-            z.st=x.st;
-            z.len=3+sz+1;
-            return z;
-        }
-        sz2=min(ask(y.l+1+sz), y.r+1)-(y.l+1+sz)-1;
-        if(sz2>=3){
-            z.ans=max(z.ans, x.st+4);
-            z.st=y.st; z.len=y.len;
-            return z;
-        }
-        if(y.l+1+sz+1+sz2-1==y.r){
-            z.st=x.st;
-            z.len=3+sz+1+sz2;
-            return z;
-        }
-        z.st=y.st;
-        z.len=y.len;
-        return z;
-    }
-    if(x.len>=3&&x.len<=x.st){
-        sz=min(ask(x.r), y.r+1)-(x.r)-1;
-        if(sz>x.st-x.len){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l+sz-1==y.r){
-            z.st=x.st;
-            z.len=x.len+sz;
-            return z;
-        }
-        if(sz!=x.st-x.len){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(a[y.l+sz]!=1){
-            z.st=y.st;
-            z.len=y.len;
-            return z;
-        }
-        if(y.l+sz==y.r){
-            z.st=x.st;
-            z.len=x.len+sz+1;
-            return z;
-        }
-        sz2=min(ask(y.l+sz), y.r+1)-(y.l+sz)-1;
-        if(sz2>=3){
-            z.ans=max(z.ans, x.st+4);
-            z.st=y.st; z.len=y.len;
-            return z;
-        }
-        if(y.l+sz+1+sz2-1==y.r){
-            z.st=x.st;
-            z.len=x.len+sz+1+sz2;
-            return z;
-        }
-        z.st=y.st;
-        z.len=y.len;
-        return z;
-    }
-    z.st=x.st; z.len=x.len;
-    sz=min(ask(x.r), y.r+1)-(x.r)-1;
-	if(sz+x.len>=x.st+4){
-		z.ans=max(z.ans, x.st+4);
-		z.st=y.st;
-		z.len=y.len;
-		return z;
-	}
-	if(y.l+sz-1==y.r){
-		z.st=x.st;
-		z.len=x.len+sz;
-		return z;
-	}
-	z.st=y.st;
-	z.len=y.len;
-    return z;
-}
+int mx[N<<2], occ[N<<2];
 void build(int p, int l, int r){
-    if(l==r){
-        tr[p].l=tr[p].r=l;
-        if(a[l]>=3) tr[p].st=a[l], tr[p].len=1;
-        else tr[p].st=0, tr[p].len=0;
-        tr[p].ans=0;
-        return ;
-    }
-    int mid=(l+r)>>1;
-    build(p<<1, l, mid); build(p<<1|1, mid+1, r);
-    tr[p]=tr[p<<1]+tr[p<<1|1];
-	// cout<<l<<' '<<r<<' '<<tr[p].st<<' '<<tr[p].len<<' '<<tr[p].ans<<endl;
+	if(l==r){
+		if(a[l]>=3) mx[p]=l;
+		else mx[p]=0;
+		if(a[l]==0) occ[p]=0;
+		else occ[p]=l;
+		return ;
+	}
+	int mid=(l+r)>>1;
+	build(p<<1, l, mid); build(p<<1|1, mid+1, r);
+	mx[p]=max(mx[p<<1], mx[p<<1|1]);
+	occ[p]=max(occ[p<<1], occ[p<<1|1]);
 }
 void mdf(int p, int l, int r, int x){
-    if(l==r){
-        tr[p].l=tr[p].r=l;
-        if(a[l]>=3) tr[p].st=a[l], tr[p].len=1;
-        else tr[p].st=0, tr[p].len=0;
-        tr[p].ans=0;
-        return ;
-    }
-    int mid=(l+r)>>1;
-    if(x<=mid) mdf(p<<1, l, mid, x);
-    else mdf(p<<1|1, mid+1, r, x);
-    tr[p]=tr[p<<1]+tr[p<<1|1];
+	if(l==r){
+		if(a[l]>=3) mx[p]=l;
+		else mx[p]=0;
+		if(a[l]==0) occ[p]=0;
+		else occ[p]=l;
+		return ;
+	}
+	int mid=(l+r)>>1;
+	if(x<=mid) mdf(p<<1, l, mid, x);
+	else mdf(p<<1|1, mid+1, r, x);
+	mx[p]=max(mx[p<<1], mx[p<<1|1]);
+	occ[p]=max(occ[p<<1], occ[p<<1|1]);
 }
-info tem; bool flag;
-void qry(int p, int l, int r, int L, int R){
-    if(L<=l&&r<=R){
-        if(!flag) flag=1, tem=tr[p];
-        else tem=tem+tr[p];
-        return ;
-    }
-    int mid=(l+r)>>1;
-    if(L<=mid) qry(p<<1, l, mid, L, R);
-    if(R>mid) qry(p<<1|1, mid+1, r, L, R);
+int get(int p, int l, int r, int L, int R){
+	if(L>R) return 0;
+	if(L<=l&&r<=R) return mx[p];
+	int mid=(l+r)>>1, ret=0;
+	if(L<=mid) ret=get(p<<1, l, mid, L, R);
+	if(R>mid) ret=max(ret, get(p<<1|1, mid+1, r, L, R));
+	return ret;
+}
+int fnd(int p, int l, int r, int L, int R){
+	if(occ[p]==0) return R+1;
+	if(L<=l&&r<=R){
+		if(l==r) return l;
+		int mid=(l+r)>>1;
+		if(occ[p<<1]!=0) return fnd(p<<1, l, mid, L, R);
+		return fnd(p<<1|1, mid+1, r, L, R);
+	}
+	int mid=(l+r)>>1, ret=R+1;
+	if(L<=mid) ret=fnd(p<<1, l, mid, L, R);
+	if(R>mid&&ret==R+1) ret=fnd(p<<1|1, mid+1, r, L, R);
+	return ret;
+}
+int getsz(int x){
+	return fnd(1, 1, n, x+1, n)-x-1;
+}
+int mx2[N<<2];
+void clr(int p, int l, int r){
+	mx2[p]=0;
+	if(l==r) return ;
+	int mid=(l+r)>>1;
+	clr(p<<1, l, mid); clr(p<<1|1, mid+1, r);
+}
+void mdf2(int p, int l, int r, int x, int v){
+	if(l==r){
+		mx2[p]=v;
+		return ;
+	}
+	int mid=(l+r)>>1;
+	if(x<=mid) mdf2(p<<1, l, mid, x, v);
+	else mdf2(p<<1|1, mid+1, r, x, v);
+	mx2[p]=max(mx2[p<<1], mx2[p<<1|1]);
+}
+int get2(int p, int l, int r, int L, int R){
+	if(L>R) return 0;
+	if(L<=l&&r<=R) return mx2[p];
+	int mid=(l+r)>>1, ret=0;
+	if(L<=mid) ret=get2(p<<1, l, mid, L, R);
+	if(R>mid) ret=max(ret, get2(p<<1|1, mid+1, r, L, R));
+	return ret;
+}
+set<int> st;
+void work(int x){
+	int i=get(1, 1, n, 1, x);
+	if(i==0) return ;
+	if(i+a[i]+3>n){
+		return ;
+	}
+	if(a[i+1]!=2){
+		return ;
+	}
+	if(a[i+2]!=1){
+		return ;
+	}
+	int sz=getsz(i+2);
+	if(sz!=a[i]-3){
+		return ;
+	}
+	if(a[i+2+sz+1]!=1){
+		return ;
+	}
+	int sz2=getsz(i+2+sz+1);
+	if(sz2<3){
+		return ;
+	}
+	st.insert(i);
+	mdf2(1, 1, n, i, a[i]+4);
 }
 void solve(){
 	read(n); read(m);
     for(int i=1; i<=n; ++i){
 		read(a[i]);
-        L[i]=R[i]=i;
     }
-    for(int i=1;i<=n;i++) add(i);
     if(n>=4){
         T1.build(1, 4, n, 1);
         T2.build(1, 4, n, 2);
@@ -354,14 +188,51 @@ void solve(){
     if(n>=5){
         T3.build(1, 5, n, 3);
     }
-    build(1, 1, n);
+	build(1, 1, n);
+	clr(1, 1, n);
+	st.clear();
+	for(int i=1; i<=n-6; ) {
+		if(a[i]<3) {
+			++i; continue;
+		}
+		if(i+a[i]+3>n){
+			++i; continue;
+		}
+		if(a[i+1]!=2){
+			++i; continue;
+		}
+		if(a[i+2]!=1){
+			++i; continue;
+		}
+		int sz=getsz(i+2);
+		if(sz!=a[i]-3){
+			++i; continue;
+		}
+		if(a[i+2+sz+1]!=1){
+			++i; continue;
+		}
+		int sz2=getsz(i+2+sz+1);
+		if(sz2<3){
+			++i; continue;
+		}
+		st.insert(i);
+		mdf2(1, 1, n, i, a[i]+4);
+		i=i+a[i]+4;
+	}
+	st.insert(0);
     int op, l, r; 
     while(m--){
 		read(op); read(l); read(r);
         if(op==1){
-            del(l);
+			set<int>::iterator it=st.upper_bound(l);
+			--it;
+			if((*it)!=0&&(*it)+a[*it]+3>=l) {
+				mdf2(1, 1, n, *it, 0);
+				st.erase(it);
+			}
             a[l]=r;
-            add(l);
+			mdf(1, 1, n, l);
+			work(l);
             if(n>=4){
                 for(int i=max(l, 4); i<=l+3&&i<=n; ++i) T1.mdf(1, 4, n, i, 1);
                 for(int i=max(l, 4); i<=l+3&&i<=n; ++i) T2.mdf(1, 4, n, i, 2);
@@ -369,7 +240,6 @@ void solve(){
             if(n>=5){
                 for(int i=max(l, 5); i<=l+4&&i<=n; ++i) T3.mdf(1, 5, n, i, 3);
             }
-            mdf(1, 1, n, l);
         }
         else{
             int ans=0;
@@ -380,9 +250,10 @@ void solve(){
             if(n>=5){
                 ans=max(ans, 5*T3.get(1, 5, n, l+4, r));
             }
-            flag=0;
-            qry(1, 1, n, l, r);
-            ans=max(ans, tem.ans);
+			set<int>::iterator it=st.upper_bound(r);
+			--it;
+			if((*it)>=l&&(*it)+a[*it]+3<=r) ans=max(ans, a[*it]+4);
+			ans=max(ans, get2(1, 1, n, l, (*it)-1));
             printf("%d\n", ans);
         }
     }
