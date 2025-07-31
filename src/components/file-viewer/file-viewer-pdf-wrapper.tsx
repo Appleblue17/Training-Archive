ed11e5c2fd4caa1ed85cbb4d64ae05739d6dbf34
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2025-07-31 21:29:03 +0800

    [update] Replace FileViewerPDFWrapper with direct FileViewerPDF usage for improved simplicity

--- src/components/file-viewer/file-viewer-pdf-wrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const FileViewerPDF = dynamic(() => import("./file-viewer-pdf"), { ssr: false });
-
-export default function FileViewerPDFWrapper({ pdfPath }: { pdfPath: string }) {
-  return <FileViewerPDF pdfPath={pdfPath} />;
-}
