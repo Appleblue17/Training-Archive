fa11a97bb03f85bcc4dda8b675ffa699364c96a0
Appleblue17 <Appleblue17@users.noreply.github.com>
2025-07-31 20:31:33 +0000

    deploy: cd632abf623ce2f07515ad58eb3626def24e839b

--- contests/2025-07-31 2025牛客暑期多校训练营6/problems/E/code.cpp
+++ contests/2025-07-31 2025牛客暑期多校训练营6/problems/E/code.cpp
@@ -3,26 +3,23 @@
 using namespace std;
 const int N=1e5+5;
 const int B=350;
-const int B2=1000;
 // const int B=2;
-// const int B2=2;
 int n, m;
-int p[N];
-int pos[N], lp[N], rp[N];
-int pos2[N], lp2[N], rp2[N];
-vector<int> vec[N];
+int p[N], revp[N];
+int cl[N], cr[N];
+int pos[N], lp[350], rp[350];
+int cnt[350][350];
 vector<int> occ[N];
-vector<int> bignum;
 typedef long long ll;
-ll atag[N], aself[N];
-ll sum[N];
+ll atag[350], aself[N];
+ll sum[350], dt[350];
 inline ll qryp(int x){
     return atag[pos[x]]+aself[x];
 }
 inline ll qry(int x){
     if(x==0) return 0;
-    ll ret=sum[pos2[x]-1];
-    for(int i=lp2[pos2[x]]; i<=x; ++i){
+    ll ret=sum[pos[x]-1];
+    for(int i=lp[pos[x]]; i<=x; ++i){
         ret+=qryp(p[i]);
     }
     return ret;
@@ -31,16 +28,31 @@ int main(){
     // freopen("D:\\nya\\acm\\A\\test.in","r",stdin);
     // freopen("D:\\nya\\acm\\A\\test.out","w",stdout);
     scanf("%d%d", &n, &m);
-    for(int i=1; i<=n; ++i) scanf("%d", &p[i]);
+    for(int i=1; i<=n; ++i) scanf("%d", &p[i]), occ[p[i]].push_back(i);
+    for(int i=1; i<=n; ++i){
+        cl[i]=cr[i-1]+1;
+        cr[i]=cr[i-1];
+        for(auto t:occ[i]){
+            ++cr[i];
+            p[t]=cr[i];
+            revp[cr[i]]=t;
+        }
+    }
     for(int i=1; i<=n; ++i) pos[i]=(i-1)/B+1;
     for(int i=1; i<=pos[n]; ++i) lp[i]=rp[i-1]+1, rp[i]=rp[i-1]+B;
-    rp[pos[n]]=1;
-    for(int i=1; i<=n; ++i) pos2[i]=(i-1)/B2+1;
-    for(int i=1; i<=pos2[n]; ++i) lp2[i]=rp2[i-1]+1, rp2[i]=rp2[i-1]+B2;
-    rp2[pos2[n]]=1;
-    for(int i=1; i<=pos2[n]; ++i){
-        for(int j=lp2[i]; j<=rp2[i]; ++j) vec[i].push_back(p[j]);
-        sort(vec[i].begin(), vec[i].end());
+    rp[pos[n]]=n;
+    for(int i=1; i<=pos[n]; ++i){
+        for(int j=1; j<=pos[n]; ++j){
+            cnt[i][j]=cnt[i][j-1];
+            for(int k=lp[i]; k<=rp[i]; ++k){
+                cnt[i][j]+=(p[k]>=lp[j]&&p[k]<=rp[j]);
+            }
+        }
+    }
+    for(int i=1; i<=pos[n]; ++i){
+        for(int j=1; j<=pos[n]; ++j){
+            cnt[i][j]+=cnt[i-1][j];
+        }
     }
     ll lstans=0;
     int tp; ll l, r, x;
@@ -50,20 +62,23 @@ int main(){
         if(tp==1){
             scanf("%lld", &x);
             x^=lstans;
+            l=cl[l]; r=cr[r];
+            if(l>r) continue;
             if(pos[l]==pos[r]){
-                for(int i=l; i<=r; ++i) aself[i]+=x;
+                for(int i=l; i<=r; ++i) aself[i]+=x, dt[pos[revp[i]]]++;
             }
             else{
-                for(int i=l; i<=rp[pos[l]]; ++i) aself[i]+=x;
-                for(int i=lp[pos[r]]; i<=r; ++i) aself[i]+=x;
+                for(int i=l; i<=rp[pos[l]]; ++i) aself[i]+=x, dt[pos[revp[i]]]++;
+                for(int i=lp[pos[r]]; i<=r; ++i) aself[i]+=x, dt[pos[revp[i]]]++;
                 for(int i=pos[l]+1; i<pos[r]; ++i) atag[i]+=x;
+                for(int i=1; i<=pos[n]; ++i) {
+                    sum[i]+=x*(cnt[i][pos[r]-1]-cnt[i][pos[l]]);
+                }
             }
-            int pre=0;
-            for(int i=1; i<=pos2[n]; ++i) {
-                int p1=upper_bound(vec[i].begin(), vec[i].end(), r)-vec[i].begin();
-                int p2=lower_bound(vec[i].begin(), vec[i].end(), l)-vec[i].begin();
-                pre+=p1-p2;
-                sum[i]+=x*pre;
+            for(int i=1; i<=pos[n]; ++i) {
+                dt[i+1]+=dt[i];
+                sum[i]+=x*dt[i];
+                dt[i]=0;
             }
         }
         else{
