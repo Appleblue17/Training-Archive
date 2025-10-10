6de9f5d69aee8a2e0608f9c9b2fe433b73462f06
Appleblue17 <Appleblue17@users.noreply.github.com>
2025-10-10 13:23:22 +0000

    deploy: cd0bafe07f20bc51ac38535064f08b2492cf1cae

--- contests/2025-09-28 Petrozavodsk Summer 2023. Day 7. PKU Contest/problems/I/code.cpp
+++ contests/2025-09-28 Petrozavodsk Summer 2023. Day 7. PKU Contest/problems/I/code.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N=1e5+5,mod=1e9+7;
+const int N=5e5+5,mod=1e9+7;
 
-int T_,n;
+int T_,n,ans;
 vector <int> G[N];
 char S[N],T[N];
 int a[N],b[N];
@@ -34,35 +34,93 @@ void gmod(int &x){
 }
 
 bool col[N];
+int son[N],siz[N];
 void dfs0(int u,int fa){
     col[u]=col[fa]^1;
+    siz[u]=1; son[u]=0;
     for(int v: G[u]){
         if(v==fa) continue;
         dfs0(v,u);
+        if(siz[v]>siz[son[u]]) son[u]=v;
+        siz[u]+=siz[v];
     }
 }
 
-int a2[N],a3[N];
-int c2[N],c3[N];
-void dfs(int u,int fa){
-    a2[u]=(a[u]==1);
-    a3[u]=(a[u]==2);
+int c,s;
+struct abc{
+    int d,w,S0,S1,T0;
+    abc(){
+        d=0,w=-1;
+        S0=S1=T0=0;
+    }
     
-    c2[u]=(b[u]==1);
-    c3[u]=(b[u]==2);
+    void addW(){
+        gmod(S0+=C(d,w+1)*C(c-d,s-w-1)%mod);
+        gmod(S1+=(w+1)*C(d,w+1)%mod*C(c-d,s-w-1)%mod);
+        gmod(T0+=C(d,w+1)*C(c-1-d,s-1-w-1)%mod);
+        w++;
+    }
+    void delW(){
+        gmod(S0+=mod-C(d,w)*C(c-d,s-w)%mod);
+        gmod(S1+=mod-w*C(d,w)%mod*C(c-d,s-w)%mod);
+        gmod(T0+=mod-C(d,w)*C(c-1-d,s-1-w)%mod);
+        w--;
+    }
+    void addD(){
+        gmod(S1+=mod-(w+1)*C(d,w)%mod*C(c-d-1,s-w-1)%mod+T0);
+        gmod(S0+=mod-C(d,w)*C(c-d-1,s-w-1)%mod);
+        if(d+1>c-1) T0=0;
+        else gmod(T0+=mod-C(d,w)*C(c-1-d-1,s-1-w-1)%mod);
+        
+        d++;
+    }
+    void upd(int d_,int w_){
+        while(w<w_) addW();
+        while(w>w_) delW();
+        while(d<d_) addD();
+    }
+}tmp,dp[N];
+
+int p[N],q[N]; // #?
+int f[N],g[N]; // sum
+void dfs(int u,int fa){
+    p[u]=(a[u]==2);
+    q[u]=(b[u]==2);
+    
+    f[u]=(a[u]==1);
+    g[u]=(b[u]!=0);
     
     for(int v: G[u]){
         if(v==fa) continue;
         dfs(v,u);
-        
-        a2[u]+=a2[v];
-        a3[u]+=a3[v];
-        c2[u]+=c2[v];
-        c3[u]+=c3[v];
+        p[u]+=p[v],q[u]+=q[v];
+        f[u]+=f[v],g[u]+=g[v];
     }
 }
 
+
+int qry(int d,int w,abc &x){
+    if(w<0) return (d*C(c-1,s-1)%mod+mod-w*C(c,s)%mod)%mod;
+    
+    x.upd(d,w);
+    int s0=x.S0,s1=x.S1;
+    return ((w*s0+mod-s1)%mod*2%mod+d*C(c-1,s-1)%mod+mod-w*C(c,s)%mod)%mod;
+}
+
+void dfss(int u,int fa){
+    for(int v: G[u]){
+        if(v==fa) continue;
+        dfss(v,u);
+    }
+    int d=p[u]+q[u],w=g[u]-f[u];
+    if(son[u]) dp[u]=dp[son[u]];
+    else dp[u]=abc();
+    int tot=qry(d,w,dp[u]);
+    gmod(ans+=tot);
+}
+
 signed main(){
+    // freopen("1.txt","w",stdout);
     init(N-5);
     
     scanf("%lld",&T_);
@@ -87,19 +145,13 @@ signed main(){
             else b[i]=(T[i]-'0')^col[i];
         }
         
+        ans=0;
         dfs(1,0);
         
-        int ans=0;
-        for(int u=1;u<=n;u++){
-            for(int t=-c3[u];t<=a3[u];t++){
-                int w=abs(a2[u]-c2[u]+t)*C(a3[u]+c3[u],t+c3[u])%mod;
-                int tot=C(a3[1]+c3[1]-a3[u]-c3[u],t+a2[1]-c2[1]+a3[1]-a3[u]);
-                gmod(ans+=tot*w%mod);
-            }
-            // cout<<u<<": "<<tot<<endl;
-        }
-        cout<<ans<<endl;
+        c=p[1]+q[1],s=g[1]-f[1];
+        dfss(1,0);
+        
+        printf("%lld\n",ans);
     }
-    
-    
 }
+
