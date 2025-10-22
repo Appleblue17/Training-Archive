02dc97c8fc0948f530f59b4ce799d2d3efb91784
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2025-10-22 18:40:23 +0800

    [manual] Manually sync

--- contests/2025-10-19 2022-2023 ICPC Southwestern Europe Regional Contest/problems/D/code.cpp
+++ contests/2025-10-19 2022-2023 ICPC Southwestern Europe Regional Contest/problems/D/code.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 #define int long long
 #define double long double
-const int N=5500;
-const double INF=1e18,eps=1e-7;
+const int N=2200,M=12,NN=3e6+5,INF=1e18;
+const double eps=1e-3;
 int n_,n,m,s,v;
 
 struct abc{
-    double a,b;
     int r;
+    double a,b;
 };
 bool operator <(abc X,abc Y){
     if(X.r==Y.r) return X.a<Y.a;
@@ -16,12 +16,10 @@ bool operator <(abc X,abc Y){
 }
 
 abc p[N];
-vector <abc> V[13];
-
-vector <int> trans[N];
+vector <abc> V[M];
 
 bool chk(int r,double pos){
-    int t=lower_bound(V[r].begin(),V[r].end(),(abc){pos-eps,pos-eps,r})-V[r].begin();
+    int t=lower_bound(V[r].begin(),V[r].end(),(abc){r,pos-eps,pos-eps})-V[r].begin();
     if(!t) return 1;
     return (pos+eps>=V[r][t-1].b);
 }
@@ -34,85 +32,146 @@ bool check(int r,int x,double k,int L,int R){
     return 1;
 }
 
-void add(int u,int v){
-    // cout<<"  add "<<u<<" "<<v<<endl;
-    trans[v].push_back(u);
+
+struct def{
+    int L,R;
+    int r,x;
+    double k;
+}q[NN];
+int id;
+
+void add(int L,int R,int r,int x,double k){
+    q[++id]={L,R,r,x,k};
+    // cout<<id<<": "<<L<<" "<<R<<" "<<r<<" "<<x<<" "<<k<<endl;
 }
 
-int dp[N];
+int dp[NN];
+struct ghi{
+    double pos1,pos2;
+    int id,typ,rk;
+}r[NN];
+bool cmp1(ghi X,ghi Y){
+    return X.pos1<Y.pos1;
+}
+bool cmp2(ghi X,ghi Y){
+    return X.pos2>Y.pos2;
+}
+int rid;
+
+
+int lowbit(int x){
+    return x & (-x);
+}
+int c[NN];
+void modify(int x,int k){
+    while(x<NN) c[x]=min(c[x],k),x+=lowbit(x);
+}
+int query(int x){
+    int tot=INF;
+    while(x) tot=min(tot,c[x]),x-=lowbit(x);
+    return tot;
+}
 
 signed main(){
     cin>>n_>>m>>s>>v;
-    V[0].push_back({-INF,0,0});
-    V[m+1].push_back({s*1.0,INF,m+1});
+    
+    V[0].push_back({0,-INF*1.0,0});
+    V[m+1].push_back({m+1,s*1.0,INF*1.0});
     for(int i=1;i<=n_;i++){
         double a,b; int r; cin>>a>>b>>r;
         if(a>=s) continue;
         b=min(b,s*1.0L);
         
-        p[++n]={a,b,r};
-        V[r].push_back(p[n]);
-    }
-    if(!n){
-        if(v*(m+1)<=s) cout<<0;
-        else cout<<-1;
-        return 0;
+        p[++n]={r,a,b};
     }
     
     sort(p+1,p+n+1);
-    for(int t=0;t<=m+1;t++) sort(V[t].begin(),V[t].end());
+    for(int i=1;i<=n;i++) V[p[i].r].push_back(p[i]);
     
-    for(int t=1;t<=n;t++){
-        int a=p[t].a,b=p[t].b,r=p[t].r;
-        for(int i=0;i<=1;i++){
-            int pos=(i==0)?a:b;
-            int id=t*2-1+i;
-            // cout<<"single "<<t<<" "<<i<<": "<<r<<" "<<pos<<" "<<id<<endl;
-            
-            if(check(r,pos,v,0,r)) add(0,id);
-            if(check(r,pos,v,r,m+1)) add(id,n*2+1);
-            if(check(r,pos,v,0,m+1)){
-                cout<<0;
-                return 0;
+    if(check(0,0,v,0,m+1)) return cout<<0,0;
+    
+    for(int i=1;i<=n;i++){
+        int a=p[i].a,r=p[i].r;
+        // cout<<"single "<<t<<" "<<i<<": "<<r<<" "<<pos<<" "<<id<<endl;
+        
+        for(int L=0;L<=m+1;L++){
+            for(int R=L+1;R<=m+1;R++){
+                if(check(r,a,v,L,R)) add(L,R,r,a,v);
             }
         }
+        if(check(r,a,v,0,m+1)) return cout<<0,0;
     }
-    for(int t=1;t<=n;t++){
-        int a=p[t].a,b=p[t].b,r=p[t].r;
-        for(int s=t+1;s<=n;s++){
-            int aa=p[s].a,bb=p[s].b,rr=p[s].r;
+    for(int i=1;i<=n;i++){
+        int b=p[i].b,r=p[i].r;
+        
+        for(int L=0;L<=m+1;L++){
+            for(int R=L+1;R<=m+1;R++){
+                if(check(r,b,v,L,R)) add(L,R,r,b,v);
+            }
+        }
+        if(check(r,b,v,0,m+1)) return cout<<0,0;
+    }
+    for(int i=1;i<=n;i++){
+        int a=p[i].a,r=p[i].r;
+        for(int j=i+1;j<=n;j++){
+            int bb=p[j].b,rr=p[j].r;
             if(r>=rr) continue;
+            if(bb<=a) continue;
+            double k=1.0*(bb-a)/(rr-r);
+            if(k<v-eps) continue;
             
-            for(int i=0;i<=1;i++){
-                for(int j=0;j<=1;j++){
-                    int pos1=(i==0)?a:b;
-                    int pos2=(j==0)?aa:bb;
-                    int id1=t*2-1+i,id2=s*2-1+j;
-                    double k=1.0*(pos2-pos1)/(rr-r);
-                    if(pos1>=pos2) continue;
-                    if(k<v-eps) continue;
-                    
-                    // cout<<"double "<<t<<" "<<i<<": "<<r<<" "<<pos1<<" "<<id1<<", "<<rr<<" "<<pos2<<" "<<id2<<endl;
-                    if(check(r,pos1,k,r,rr)) add(id1,id2);
-                    if(check(r,pos1,k,0,rr)) add(0,id2);
-                    if(check(r,pos1,k,r,m+1)) add(id1,n*2+1);
-                    
-                    if(check(r,pos1,k,0,m+1)){
-                        cout<<0;
-                        return 0;
-                    }
+            // cout<<"double "<<t<<" "<<i<<": "<<r<<" "<<pos1<<" "<<id1<<", "<<rr<<" "<<pos2<<" "<<id2<<endl;
+            if(check(r,a,k,r,rr)) add(r,rr,r,a,k);
+            if(check(r,a,k,0,rr)) add(0,rr,r,a,k);
+            if(check(r,a,k,r,m+1)) add(r,m+1,r,a,k);
+            
+            if(check(r,a,k,0,m+1)) return cout<<0,0;
+        }
+    }
+    
+    memset(dp,0x3f,sizeof(dp));
+    for(int i=1;i<=id;i++){
+        if(q[i].L==0) dp[i]=0;
+    }
+    
+    // for(int i=1;i<=id;i++) cout<<dp[i]<<" ";
+    // cout<<endl;
+    for(int t=0;t<m;t++){
+        // t -> t+1
+        
+        for(int fl=0;fl<=1;fl++){
+            rid=0;
+            for(int i=1;i<=id;i++){
+                double pos1=q[i].x+(t-q[i].r)*q[i].k;
+                double pos2=q[i].x+(t+1-q[i].r)*q[i].k;
+                if(fl) pos1=-pos1,pos2=-pos2;
+                if(q[i].L<=t && q[i].R>=t){
+                    r[++rid]={pos1,pos2,i,0};
+                }
+                if(q[i].L<=t+1 && q[i].R>=t+1){
+                    r[++rid]={pos1+eps,pos2-eps,i,1};
                 }
             }
+            
+            sort(r+1,r+rid+1,cmp1);
+            for(int i=1;i<=rid;i++) r[i].rk=i;
+            sort(r+1,r+rid+1,cmp2);
+            
+            for(int i=1;i<=rid;i++) c[i]=INF;
+            for(int i=1;i<=rid;i++){
+                int id=r[i].id,rk=r[i].rk;
+                if(r[i].typ) dp[id]=min(dp[id],query(rk)+1);
+                else modify(rk,dp[id]);
+            }
         }
+        // cout<<endl;
+        // for(int i=1;i<=id;i++) cout<<dp[i]<<" ";
+        // cout<<endl;
     }
     
-    for(int i=1;i<=n*2+1;i++){
-        dp[i]=INF;
-        for(int j: trans[i]){
-            dp[i]=min(dp[i],dp[j]+1);
-        }
-        // cout<<i<<": "<<dp[i]<<endl;
-    }
-    if(dp[n*2+1]>=1e9) cout<<-1;
-    else cout<<dp[n*2+1]-1;
+    int ans=INF;
+    for(int i=1;i<=id;i++)
+        if(q[i].R==m+1) ans=min(ans,dp[i]);
+    if(ans==INF) cout<<-1;
+    else cout<<ans;
 }
